ce753463c96f485fea7f13e4e22ba58e4191449c
Meldrick Trujeque <meldrick.dev22@gmail.com>
2023-09-13 06:57:49 -0600

    Configuraciones del proyecto

---
+++ app/src/main/java/com/matb/basicpokedex/PokedexApp.kt
@@ -0,0 +1,11 @@
+package com.matb.basicpokedex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by meldricktb$ on 13/09/23$.
+ */
+@HiltAndroidApp
+class PokedexApp : Application() {
+}
